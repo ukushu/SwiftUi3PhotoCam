ae3f5a8c623195adbab3f6d574071b20eb52f1b8
UKS <skulptorr@gmail.com>
2022-08-06 11:20:03 +0300

    ToDo

---
+++ ToDo.swift
@@ -0,0 +1,23 @@
+/*
+
+ * Mirroring screen
+ * Horisontal teleprompter
+ * Fix UI to better
+ * Controlling prompter from another device
+ * Scroll related to dictation of the text
+ * Три режими: пауз-камера, промтптер, Промтер + відеозйомка
+ * Bionic Reading
+ * Відкриття файлів з дропбокса, гуглдрайва, тощо (txt, rtf, word)
+ * 3, 2, 1 перед записом
+ * виставляння світла тапом по екрану
+ *
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+*/
