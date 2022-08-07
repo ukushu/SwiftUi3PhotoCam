102d2ec684bdff5bf22b011d3c87e36d06bf5ae7
UKS <skulptorr@gmail.com>
2022-08-07 07:50:13 +0300

    ToDo upd

--- ToDo.swift
+++ ToDo.swift
@@ -1,11 +1,7 @@
 /*
-
- * Fix UI to better
  * підтримка горизонтальної орієнтації
  * контакт саппорт
  * зберігати налаштування кольору
- * виставляння світла тапом по екрану
-
  
  VideoSettings:
  * Choose camera (front is default)
@@ -13,11 +9,11 @@
  * Video frame rate
  * Countdown timer: 3, 2, 1 перед записом
  
- 
  Far-far away update
  * Bionic Reading
  * Відкриття файлів з дропбокса, гуглдрайва, тощо (txt, rtf, word)
 
+ * виставляння світла тапом по екрану
  * Controlling prompter from another device
  * ремоут контрол: через гейм-контроллер, блютуз-клаву, через інший епл девайс
  * Scroll related to dictation of the text
